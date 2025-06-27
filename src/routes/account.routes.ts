83eccfc162242753ea623cb091c855b38c144ee5
Hosea Kibe Nganga <hoseanganga27@gmail.com>
2025-06-27 15:04:45 +0300

    Added Endpoints for a single account and transaction

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,10 +1,23 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { accounts } from "../data/accounts";
+import { Account } from "../models/account.model";
 
 const router = Router();
 
 router.get("/", (_req, res) => {
   res.json(accounts);
+});
+
+router.get("/:id", async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
+  const account = accounts.find((acc) => acc.id === id);
+
+  if (!account) {
+    res.status(404).json({ message: "Account not found" });
+    return;
+  }
+
+  res.json(account);
 });
 
 export default router;
